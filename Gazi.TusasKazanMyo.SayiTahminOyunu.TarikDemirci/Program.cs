37c17ce5fd5475de99db10547a75a936079d5796
tarikdemirci61 <76911172+tarikdemirci61@users.noreply.github.com>
2021-01-03 22:04:00 +0300

    Add project files.

---
+++ Gazi.TusasKazanMyo.SayiTahminOyunu.TarikDemirci/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gazi.TusasKazanMyo.SayiTahminOyunu.TarikDemirci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        BASADON:
+            Console.WriteLine("SEVİYE SEÇİNİZ (KOLAY-ORTA-ZOR):");
+            string seviye = Console.ReadLine().ToLower();
+
+            for (; ; )
+            {
+                if (seviye == "kolay")
+                {
+                    int hak = 4;
+                    Random rnd = new Random();
+                    int sayi = rnd.Next(1, 10);
+                TEKRAR:
+                    Console.WriteLine("Bir tahmin yapınız:");
+                    int tahmin = int.Parse(Console.ReadLine());
+                    if (hak <= 0)
+                    {
+                        Console.WriteLine("Hakkınız Bitti!");
+                        goto BASADON;
+                    }
+                    else if (sayi == tahmin)
+                    {
+                        Console.WriteLine("Tebrikler... Bildiniz");
+                        goto BASADON;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tekrar Deneyiniz...Kalan Hakkınız:" + hak);
+                        hak--;
+                        goto TEKRAR;
+                    }
+                }
+                else if (seviye == "orta")
+                {
+                    int hak = 4;
+                    Random rnd = new Random();
+                    int sayi = rnd.Next(1, 25);
+                TEKRAR:
+                    Console.WriteLine("Bir tahmin yapınız:");
+                    int tahmin = int.Parse(Console.ReadLine());
+                    if (hak <= 0)
+                    {
+                        Console.WriteLine("Hakkınız Bitti!");
+                        goto BASADON;
+                    }
+                    else if (sayi == tahmin)
+                    {
+                        Console.WriteLine("Tebrikler... Bildiniz");
+                        goto BASADON;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tekrar Deneyiniz...Kalan Hakkınız:" + hak);
+                        hak--;
+                        goto TEKRAR;
+                    }
+                }
+                else if (seviye == "zor")
+                {
+                    int hak = 5;
+                    Random rnd = new Random();
+                    int sayi = rnd.Next(1, 50);
+                TEKRAR:
+                    Console.WriteLine("Bir tahmin yapınız:");
+                    int tahmin = int.Parse(Console.ReadLine());
+                    if (hak <= 0)
+                    {
+                        Console.WriteLine("Hakkınız Bitti!");
+                        goto BASADON;
+                    }
+                    else if (sayi == tahmin)
+                    {
+                        Console.WriteLine("Tebrikler... Bildiniz");
+                        goto BASADON;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tekrar Deneyiniz...Kalan Hakkınız:" + hak);
+                        hak--;
+                        goto TEKRAR;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Sadece (KOLAY-ORTA-ZOR) seviyelerinden birini yazabilirsiniz");
+                    goto BASADON;
+                }
+            }
+        }
+    }
+}
